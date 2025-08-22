e84365634f6ba31e9623bfa57fa6596efebba073
Alekhya Nakka <alekhyanakka98@gmail.com>
2025-08-22 12:19:16 -0400

    Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'build' ? '/Alekhya_Portfolio/' : '/',
+    base: command === 'build' ? '/Portfolio/' : '/',
     plugins: [react()],
   };
 });
